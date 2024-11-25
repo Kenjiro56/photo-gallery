f924927a4940feff6e845f29ef3b9be36f4dd348
Kenjiro56 <hiroken506@gmail.com>
2024-11-26 07:53:13 +0900

    intro/microCMS

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,27 @@
 'use client';
-import React, { useState, useEffect } from 'react';
-import ImageTile from './components/imageTile';
-import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { client, getList } from '../libs/microcms';
+
+interface Gallery {
+  id: string;
+  photos: string;
+}
+
 
 
 export default function Home() {
-  const dummyData = [
-    {
-      src: 'dummy/dummy1.jpg',
-    },
-    {
-      src: 'dummy/dummy2.jpg',
-    },
-    {
-      src: 'dummy/dummy3.jpg',
-    },
-    {
-      src: 'dummy/dummy4.jpg',
-    },
-    {
-      src: 'dummy/dummy5.jpg',
-    },
-  ]
+  const [images, setImages] = useState([]);
+  client
+  .get({
+    endpoint: 'getimages',
+    contentId: process.env.NEXT_PUBLIC_CONTENT_ID,
+  })
+  .then((res) => console.log(res));
 
-  const router = useRouter();
-
-  const handlePageMove = () => {
-    router.push('/map');
-  }
-
-  const fetchAlbums = async () => {
-
-    try{
-      const response = await fetch('/api/GoogleApi', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ access_token: process.env.GOOGLE_REFRESH_TOKEN }),
-      });
-
-        const data = await response.json();
-        console.log('data:', data);
-      } catch (error) {
-        console.error('Error fetching albums:', error);
-      }
-    };
 
   return (
-    <>
-      <ImageTile
-        data={dummyData}
-      />
-      <button onClick={fetchAlbums}>Fetch Albums</button>
-    </>
-
+  <div>
+    <h1>Gallery</h1>
+  </div>
   );
 }
