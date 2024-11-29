263ca5a64f5122bf882a6506e0027bbc629a5592
Kenjiro56 <hiroken506@gmail.com>
2024-11-29 16:55:11 +0900

    fix/microcms

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -16,12 +16,12 @@ if (!process.env.MICROCMS_SERVICE_DOMAIN) {
  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
 }
 
-if (!process.env.API_KEY) {
+if (!process.env.MICROCMS_API_KEY) {
  throw new Error("MICROCMS_API_KEY is required");
 }
 
 // API取得用のクライアントを作成
 export const client = createClient({
  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
- apiKey: process.env.API_KEY,
+ apiKey: process.env.MICROCMS_API_KEY,
 });
