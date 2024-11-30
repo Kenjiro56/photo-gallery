a6c2afa032b39c69ab2d28c0d0831811bec2e033
Kenjiro56 <hiroken506@gmail.com>
2024-12-01 00:36:57 +0900

    rm/unusedImport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { UIProvider } from "@yamada-ui/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
