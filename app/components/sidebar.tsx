66dd146b1e1c2312b773041ced04f4d16cc00370
Kenjiro56 <hiroken506@gmail.com>
2024-12-09 23:49:30 +0900

    fix/grid_layout

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { HouseIcon, ImagesIcon } from "@yamada-ui/lucide"
-import { Link, Flex, Box, Text } from '@yamada-ui/react';
+import { Link, Flex, Box, Text, useBreakpointValue } from '@yamada-ui/react';
 import { Sawarabi_Gothic } from "next/font/google";
 const SawarabiGothicFont = Sawarabi_Gothic({
   weight: "400",
@@ -10,33 +10,34 @@ const SawarabiGothicFont = Sawarabi_Gothic({
 
 
 const SideBar = () => {
+  const isMobile = useBreakpointValue({ base: true, lg: false })
   return (
-    <Box
-      position='fixed'
-      left={0}
-      top={0}
-      w='8%'
-      h="100%"
-    >
-      <Flex direction="column" alignItems="left" gap='3' p='8'>
-          <Link href="/">
-            <Flex gap='1'>
-              <HouseIcon fontSize={{lg: "3xl" }} color='#333'  />
-              <Text display={{ base: "block", md: "none" }} className={ SawarabiGothicFont.className } color='#333'>
-                Home
-              </Text>
-            </Flex>
-          </Link>
-          <Link href="gallery">
-            <Flex gap='1'>
-              <ImagesIcon fontSize={{ lg: "3xl" }} color='#333' />
-              <Text display={{base: "block", md: "none" }} className={ SawarabiGothicFont.className } color='#333'>
-                Gallery
-              </Text>
-            </Flex>
-          </Link>
-      </Flex>
-    </Box>
+      <Box
+        position='fixed'
+        left={0}
+        top={0}
+        w='8%'
+        h="100%"
+      >
+        <Flex direction="column" alignItems="left" gap='3' p='8'>
+            <Link href="/">
+              <Flex gap='1'>
+                <HouseIcon fontSize={{lg: "3xl" }} color='#333'  />
+                <Text display={{ base: "block", md: "none" }} className={ SawarabiGothicFont.className } color='#333'>
+                  Home
+                </Text>
+              </Flex>
+            </Link>
+            <Link href="gallery">
+              <Flex gap='1'>
+                <ImagesIcon fontSize={{ lg: "3xl" }} color='#333' />
+                <Text display={{base: "block", md: "none" }} className={ SawarabiGothicFont.className } color='#333'>
+                  Gallery
+                </Text>
+              </Flex>
+            </Link>
+        </Flex>
+      </Box>
   );
 }
 
