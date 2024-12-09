a4739539f5ae72a89ece5e791872ca446d5bc3d7
Kenjiro56 <hiroken506@gmail.com>
2024-12-09 16:47:31 +0900

    fix/button

--- app/components/detailModal.tsx
+++ app/components/detailModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Button, CardBody, Center, CardFooter, Text, Tag } from '@yamada-ui/react';
+import { Card, Image, Button, CardBody, Center, CardFooter, Text, Tag, VStack, Flex } from '@yamada-ui/react';
 import { Zen_Kurenaido } from "next/font/google";
 import { MicroCMSPhoto } from '@/types/microcmstype';
 
@@ -17,19 +17,21 @@ const DetailModal: React.FC<DetailModalProps> = (props) => {
   return (
     <div className="fixed z-10 top-0 left-0 w-full h-full bg-black bg-opacity-50">
       <Center className='h-screen items-center'>
-        <Card backgroundColor='white' position='relative' width='70%'>
+        <Card backgroundColor='white' position='relative' maxWidth='70%' maxHeight='70%' width='auto' height='auto'>
           <CardBody>
             <Image src={props.imageInfo.image.url} alt="photo" />
           </CardBody>
-          <CardFooter justifyContent="center">
-            <Tag>
+          <CardFooter justifyContent="center" width='100%'>
+          <Flex align='center' direction='column' gap='2'>
+            <Tag as={Center}>
               { props.imageInfo.category[0] }
             </Tag>
             <Text className={ZenKurenaidoFont.className}>
               { props.imageInfo.comment }
             </Text>
+            <Button onClick={props.onClose} textColor='black'>Close</Button>
+          </Flex>
           </CardFooter>
-          <Button onClick={props.onClose} textColor='black'>Close</Button>
         </Card>
       </Center>
     </div>
